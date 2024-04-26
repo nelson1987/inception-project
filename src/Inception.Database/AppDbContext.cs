9ace80da901e803bb956ef85bd2d27eb25fe807b
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-04-26 17:23:47 -0300

    Change database entities

--- src/Inception.Database/AppDbContext.cs
+++ src/Inception.Database/AppDbContext.cs
@@ -8,8 +8,8 @@ public partial class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     { }
-    public virtual DbSet<Produto> Produtos { get; set; }
-    public virtual DbSet<Usuario> Usuarios { get; set; }
+    public virtual DbSet<Empregado> Produtos { get; set; }
+    public virtual DbSet<Endereco> Enderecos { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
@@ -20,29 +20,40 @@ public partial class AppDbContext : DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Produto>(entity =>
+        modelBuilder.Entity<Empregado>(entity =>
         {
             entity.HasKey(e => e.Id);
-            entity.Property(e => e.Imagem).HasMaxLength(250);
             entity.Property(e => e.Nome)
                 .IsRequired()
                 .HasMaxLength(100);
-            entity.Property(e => e.Preco).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Imagem)
+                .HasMaxLength(250);
+            entity.Property(e => e.Nascimento)
+                .HasColumnType("date");
+            entity.Property(e => e.Salario)
+                .HasColumnType("decimal(18, 2)");
         });
-        modelBuilder.Entity<Usuario>(entity =>
+        modelBuilder.Entity<Endereco>(entity =>
         {
             entity.HasKey(e => e.Id);
-            entity.Property(e => e.Email).HasMaxLength(250);
-            entity.Property(e => e.Login)
-                .IsRequired()
-                .HasMaxLength(80);
-            entity.Property(e => e.Nome)
-                .IsRequired()
-                .HasMaxLength(100);
-            entity.Property(e => e.Senha)
-                .IsRequired()
-                .HasMaxLength(80);
+            entity.Property(e => e.Rua)
+                .HasMaxLength(250);
         });
+        //modelBuilder.Entity<Usuario>(entity =>
+        //{
+        //    entity.HasKey(e => e.Id);
+        //    entity.Property(e => e.Email)
+        //        .HasMaxLength(250);
+        //    entity.Property(e => e.Login)
+        //        .IsRequired()
+        //        .HasMaxLength(80);
+        //    entity.Property(e => e.Nome)
+        //        .IsRequired()
+        //        .HasMaxLength(100);
+        //    entity.Property(e => e.Senha)
+        //        .IsRequired()
+        //        .HasMaxLength(80);
+        //});
         OnModelCreatingPartial(modelBuilder);
     }
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
--- src/Inception.Database/Program.cs
+++ src/Inception.Database/Program.cs
@@ -3,6 +3,6 @@ using Inception.Database;
 
 Console.WriteLine("Hello, World!");
 AppDbContext context = new AppDbContext();
-await context.Produtos.AddAsync(new Produto());
-await context.Usuarios.AddAsync(new Usuario());
+await context.Produtos.AddAsync(new Empregado());
+await context.Enderecos.AddAsync(new Endereco());
 await context.SaveChangesAsync();

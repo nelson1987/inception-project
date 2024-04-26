873cb4cb7bc1bfbb5ad90ddb8843809d4b8065c8
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-04-26 15:15:14 -0300

    enable annotations

--- src/Inception.Api/Features/Empregados/EmpregadosController.cs
+++ src/Inception.Api/Features/Empregados/EmpregadosController.cs
@@ -19,12 +19,13 @@ namespace Inception.Api.Features.Empregados
 
         //GETALL
         [HttpGet(Name = "Get All Empregado")]
-        [ProducesResponseType(200)]
-        [SwaggerResponse(200, "The product was created")]
-        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
-        [SwaggerResponse(200, "The product was created")]
-        [ProducesResponseType(500)]
         [SwaggerOperation("Get all empregado", "Requires admin privileges")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(500)]
+        [SwaggerResponse(200, "The product was created")]
+        [SwaggerResponse(400, "The product was created")]
+        [SwaggerResponse(500, "The product was created")]
         public async Task<ActionResult> GetAll()
         {
             return Unauthorized();
@@ -33,12 +34,15 @@ namespace Inception.Api.Features.Empregados
 
         //GETBYID
         [HttpGet("{id:int}", Name = "Get Empregado By Id")]
-        [ProducesResponseType(typeof(Empregado), 201)]
-        [SwaggerResponse(201, "The product was created", typeof(Empregado))]
+        [SwaggerOperation("Get empregado by id", "Requires admin privileges")]
+        [ProducesResponseType(typeof(Empregado), 200)]
         [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
         [ProducesResponseType(404)]
         [ProducesResponseType(500)]
-        [SwaggerOperation("Get empregado by id", "Requires admin privileges")]
+        [SwaggerResponse(200, "The product was created", typeof(Empregado))]
+        [SwaggerResponse(400, "The product was created", typeof(IDictionary<string, string>))]
+        [SwaggerResponse(404, "The product was created")]
+        [SwaggerResponse(500, "The product was created")]
         public async Task<ActionResult> GetById([FromRoute] int id)
         {
             return Unauthorized();
@@ -49,6 +53,8 @@ namespace Inception.Api.Features.Empregados
         //POST
         [HttpPost(Name = "Create Empregado")]
         [SwaggerOperation("Creates a new empregado", "Requires admin privileges")]
+        [ProducesResponseType(typeof(Empregado), 201)]
+        [SwaggerResponse(201, "The product was created", typeof(Empregado))]
         public async Task<ActionResult> Post([FromBody, BindRequired] Empregado empregado)
         {
             if (!ModelState.IsValid)
@@ -61,6 +67,8 @@ namespace Inception.Api.Features.Empregados
         //PUT
         [HttpPut("{id:int}", Name = "Update Empregado")]
         [SwaggerOperation("Update a Empregado", "Requires admin privileges")]
+        [ProducesResponseType(typeof(Empregado), 201)]
+        [SwaggerResponse(201, "The product was created", typeof(Empregado))]
         public async Task<ActionResult> Update(int id, Empregado empregado)
         {
             return Unauthorized();
@@ -71,6 +79,8 @@ namespace Inception.Api.Features.Empregados
         //DELETE
         [HttpDelete(Name = "Delete Empregado")]
         [SwaggerOperation("Delete a Empregado", "Requires admin privileges")]
+        [ProducesResponseType(204)]
+        [SwaggerResponse(204, "The empregado was deleted")]
         public async Task<ActionResult> Delete(int id)
         {
             return Unauthorized();
--- src/Inception.Api/Program.cs
+++ src/Inception.Api/Program.cs
@@ -1,10 +1,6 @@
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using System.Reflection;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// https://github.com/domaindrivendev/Swashbuckle.AspNetCore
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,11 +8,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.EnableAnnotations();
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-    c.CustomOperationIds(apiDesc =>
-    {
-        return apiDesc.TryGetMethodInfo(out MethodInfo methodInfo) ? methodInfo.Name : null;
-    });
 });
 
 var app = builder.Build();
@@ -25,11 +16,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoAPI V1");
-        c.RoutePrefix = string.Empty;
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();

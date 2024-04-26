f4588f4195eb4c689419845874b67c5e8d9d7033
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-04-26 18:15:33 -0300

    Include Annotations

--- src/Inception.Api/Program.cs
+++ src/Inception.Api/Program.cs
@@ -12,10 +12,7 @@ builder.Services.AddControllers()
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.EnableAnnotations();
-});
+builder.Services.AddSwaggerGen(c => { c.EnableAnnotations(); });
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddScoped<IValidator<CreateEmpregadoRequest>, CreateEmpregadoValidator>();
 

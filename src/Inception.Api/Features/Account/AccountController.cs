fca01ac7a0368d3207b5c9e88f0250135be4a5a0
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-04-30 20:30:00 -0300

    add authentication with persistence

---
+++ src/Inception.Api/Features/Account/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Inception.Api.Features.Account;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
+{
+    [HttpPost]
+    [Route("login")]
+    [AllowAnonymous]
+    public ActionResult<dynamic> Authenticate([FromBody] User model)
+    {
+        var user = UserRepository.Get(model.Username, model.Password);
+
+        if (user == null)
+            return NotFound(new { message = "Usuário ou senha inválidos" });
+
+        var token = TokenService.GenerateToken(user);
+        user.Password = "";
+        return new
+        {
+            user,
+            token
+        };
+    }
+
+    [HttpGet]
+    [Route("anonymous")]
+    [AllowAnonymous]
+    public string Anonymous()
+    {
+        return "Anônimo";
+    }
+
+    [HttpGet]
+    [Route("authenticated")]
+    [Authorize]
+    public string Authenticated()
+    {
+        var identity = (ClaimsIdentity?)User.Identity!;
+        var roles = identity.Claims
+            .Where(c => c.Type == ClaimTypes.Role)
+            .Select(c => c.Value);
+
+        return $"Autenticado - {identity.Name} Role: {string.Join(",", roles.ToList())}";
+    }
+
+    [HttpGet]
+    [Route("employee")]
+    [Authorize(Roles = "employee,manager")]
+    public string Employee()
+    {
+        var identity = (ClaimsIdentity?)User.Identity!;
+        return $"Funcionário{identity.Name}";
+    }
+
+    [HttpGet]
+    [Route("manager")]
+    [Authorize(Roles = "manager")]
+    public string Manager()
+    {
+        var identity = (ClaimsIdentity?)User.Identity!;
+        return $"Gerente{identity.Name}";
+    }
+}
--- src/Inception.Api/Program.cs
+++ src/Inception.Api/Program.cs
@@ -1,20 +1,45 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Inception.Api.Contracts;
+using Inception.Api.Features.Account;
 using Inception.Api.Features.Empregados.Create;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// https://github.com/domaindrivendev/Swashbuckle.AspNetCore
-
+builder.Services.AddCors();
 builder.Services.AddControllers()
                 .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => { c.EnableAnnotations(); });
+builder.Services.AddSwaggerGen(c => { 
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+        "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+        "Example: \"Bearer 1safsfsdfdfd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddScoped<IValidator<CreateEmpregadoRequest>, CreateEmpregadoValidator>();
+builder.Services.AddUserAuthentication();
 
 var app = builder.Build();
 
@@ -26,9 +51,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();

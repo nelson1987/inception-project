b2976bde7ba8ae404abbea06d806fd5ce64a05b0
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-05-02 14:01:19 -0300

    create a k6 tests

--- src/Inception.Api/Features/Empregados/EmpregadosController.cs
+++ src/Inception.Api/Features/Empregados/EmpregadosController.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Swashbuckle.AspNetCore.Annotations;
 using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 
 namespace Inception.Api.Features.Empregados;
 
@@ -23,12 +24,10 @@ namespace Inception.Api.Features.Empregados;
 public class EmpregadosController : ControllerBase
 {
     private readonly ILogger<EmpregadosController> _logger;
-    private readonly IAppDbContext _context;
 
-    public EmpregadosController(ILogger<EmpregadosController> logger, IAppDbContext context)
+    public EmpregadosController(ILogger<EmpregadosController> logger)
     {
         _logger = logger;
-        _context = context;
     }
 
     //GETALL
@@ -40,11 +39,19 @@ public class EmpregadosController : ControllerBase
     [SwaggerResponse(200, "The product was created")]
     [SwaggerResponse(400, "The product was created")]
     [SwaggerResponse(500, "The product was created")]
-    public async Task<ActionResult> GetAll([FromServices] IEmpregadoGetAllIdHandler handler,
+    public async Task<ActionResult> GetAll(//[FromServices] IEmpregadoGetAllIdHandler handler,
     CancellationToken cancellationToken = default)
     {
+        string input = "codemaze is awesome";
+        var reverse = new StringBuilder(input.Length);
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reverse.Append(input[i]);
+        }
+        Thread.Sleep(500);
+        //return reverse.ToString();
         //return Unauthorized();
-        return Ok(await _context.Produtos.ToListAsync(cancellationToken));
+        return Ok(reverse.ToString());// await _context.Produtos.ToListAsync(cancellationToken));
     }
 
     //GETBYID
@@ -64,7 +71,7 @@ public class EmpregadosController : ControllerBase
     {
         //return Unauthorized();
         //return NotFound();
-        return Ok(await _context.Produtos.FirstOrDefaultAsync(x => x.Id == id, cancellationToken));
+        return Ok();//await _context.Produtos.FirstOrDefaultAsync(x => x.Id == id, cancellationToken));
     }
 
     //POST

fbcad2d04202b61bb7ae368913b0a7afb59f8f76
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-05-02 14:49:25 -0300

    Add Unit Tests by tdd

---
+++ src/Inception.Core/Movimentacao.cs
@@ -0,0 +1,10 @@
+﻿namespace Inception.Core
+{
+    public class Movimentacao
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataMovimentacao { get; set; }
+        public ContaBancaria Conta { get; set; }
+    }
+}

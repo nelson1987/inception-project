f09bf237ed9f8d88d4b2388a28c8ad5873b7495d
Nelson Neto <nelson.ash@outlook.com>
2024-05-03 14:27:50 -0300

    add authentication with persistence (#9)
    * add authentication with persistence
    
    * Create requests and return by CI
    
    * :lock: Add JwtToken
    
    * :lock: Change Controller return
    
    ---------
    
    Co-authored-by: Nelson Da Silva Neto <nelson.neto@stone.com.br>

--- src/Inception.Api/Program.cs
+++ src/Inception.Api/Program.cs
@@ -1,31 +1,50 @@
 using FluentValidation;
 using Inception.Api.Contracts;
+using Inception.Api.Features.Account;
 using Inception.Api.Features.ContasBancarias;
 using Inception.Api.Features.Empregados;
 using Inception.Api.Features.Empregados.Create;
+using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.RateLimiting;
 using Swashbuckle.AspNetCore.Filters;
 using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddContaBancaria();
-// https://github.com/domaindrivendev/Swashbuckle.AspNetCore
-
+builder.Services.AddCors();
+builder.Services.AddContaBancaria()
+                .AddScoped<IValidator<CreateEmpregadoRequest>, CreateEmpregadoValidator>();
+builder.Services.AddUserAuthentication();
 builder.Services.AddControllers()
                 .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerExamplesFromAssemblyOf(typeof(WeatherForecastRequestExample));
-builder.Services.AddSwaggerGen(c =>
-{
-    c.ExampleFilters();
+builder.Services.AddSwaggerExamplesFromAssemblyOf(typeof(WeatherForecastResponseExample));
+builder.Services.AddSwaggerGen(c => { 
     c.EnableAnnotations();
+    c.ExampleFilters();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+        "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+        "Example: \"Bearer 1safsfsdfdfd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
 });
-//builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddScoped<IValidator<CreateEmpregadoRequest>, CreateEmpregadoValidator>();
 
 builder.Services.AddRateLimiter(rateLimiterOptions =>
 {
@@ -61,12 +80,9 @@ app.UseSwaggerUI();
 // }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
-
 app.UseExceptionHandler(appError =>
 {
     appError.Run(async context =>
@@ -86,7 +102,5 @@ app.UseExceptionHandler(appError =>
         }
     });
 });
-
 app.UseRateLimiter();
-
 app.Run();

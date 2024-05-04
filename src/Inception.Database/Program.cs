87512ce3b53c8e0d61cb2d98a2485ef053a8c55e
Nelson Neto <nelson.ash@outlook.com>
2024-05-04 11:28:19 -0300

    refactor entityframework local

--- src/Inception.Database/Program.cs
+++ src/Inception.Database/Program.cs
@@ -1,8 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Inception.Database;
-
 Console.WriteLine("Hello, World!");
-AppDbContext context = new AppDbContext();
-await context.Produtos.AddAsync(new Empregado());
-await context.Enderecos.AddAsync(new Endereco());
-await context.SaveChangesAsync();
+//AppDbContext context = new AppDbContext();
+//await context.Produtos.AddAsync(new Empregado());
+//await context.Enderecos.AddAsync(new Endereco());
+//await context.SaveChangesAsync();
